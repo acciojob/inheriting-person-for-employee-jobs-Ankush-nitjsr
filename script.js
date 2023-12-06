91f634004120c1222044854e854eee79796718dc
Ankush <125112667+Ankush-nitjsr@users.noreply.github.com>
2023-12-06 17:17:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // complete this js code
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
 Person.prototype.greet = function() {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
