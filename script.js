cd257155767bb74b8a46fbad7f14b3a34245c071
Ankush <125112667+Ankush-nitjsr@users.noreply.github.com>
2023-12-06 17:14:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 // complete this js code
-function Person(name, age) {
-	this.name = name;
-	this.age = age;
+Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+// Constructor function for Employee, inheriting from Person
+function Employee(name, age, jobTitle) {
+    // Call the Person constructor to initialize inherited properties
+    Person.call(this, name, age);
+
+    // Add properties specific to Employee
+    this.jobTitle = jobTitle;
 }
 
-Person.prototype.greet = function() {
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-};
-
-function Employee(name, age, jobTitle) {
-	Person.call(this, name, age);
-	this.jobTitle = jobTitle;
-};
-
+// Inherit from Person by setting the prototype of Employee
 Employee.prototype = Object.create(Person.prototype);
 
-Employee.prototype.jonGreet = function)() {
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+// Method added to the Employee prototype
+Employee.prototype.jobGreet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
 // Do not change code below this line
